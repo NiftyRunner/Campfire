7ce5d6fba626a2f8ff13f8d4d8f22aae7b7336de
NiftyRunner <parthverma103@gmail.com>
2025-12-29 18:31:46 +0530

    created relay

---
+++ Assets/Scripts/Relay/RelayHandler.cs
@@ -0,0 +1,37 @@
+using System.Threading.Tasks;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using Unity.Netcode;
+using Unity.Networking.Transport.Relay;
+using UnityEngine;
+using Unity.Netcode.Transports.UTP;
+
+public class RelayHandler : MonoBehaviour
+{
+    public static RelayHandler Instance {  get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public async Task<string> CreateRelay()
+    {
+        try
+        {
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(3);
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(AllocationUtils.ToRelayServerData(allocation, "dtls"));
+
+            NetworkManager.Singleton.StartHost();
+
+            return joinCode;
+        }
+        catch (RelayServiceException e) {
+            Debug.Log(e);
+            return null;
+        }
+    }
+
+}
